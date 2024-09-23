07440bdc9f36dfaf3c6b9d8f352e8cd0de470378
Tay Johnny <tayjohnny@Tays-MacBook-Pro.local>
2024-09-24 00:20:56 +0800

    update pipeline files

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,17 +1,22 @@
 import subprocess
+import logging
 
 def run_pipeline():
     # Run model_training.py
-    subprocess.run(["python", "model_training.py"], check=True)
+    logging.info("Running model_training.py")
+    subprocess.run(["python", "-m", "src.model_training"], check=True)
     
     # Run evaluation.py
-    subprocess.run(["python", "evaluation.py"], check=True)
-    
+    logging.info("Running evaluation.py")
+    subprocess.run(["python", "-m", "src.evaluation"], check=True)
+
     # Run reddit_keywords.py
-    subprocess.run(["python", "reddit_keywords.py"], check=True)
+    logging.info("Running reddit_keywords.py")
+    subprocess.run(["python", "-m", "src.extract_reddit_keywords"], check=True)
 
     # Run get_trending_keywords.py
-    subprocess.run(["python", "get_trending_keywords.py"], check=True)
+    logging.info("Running get_trending_keywords.py")
+    subprocess.run(["python", "-m", "src.get_trending_keywords"], check=True)
 
 if __name__ == "__main__":
     '''python -m src.pipeline'''
