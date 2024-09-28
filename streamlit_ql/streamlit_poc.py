5feb046932d5888d019785612ad7f34caa689184
creole12138 <creole12138@gmail.com>
2024-09-28 15:14:48 +0800

    added a streamlit UI with date and sub reddit selection

---
+++ streamlit_ql/streamlit_poc.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+
+# Sample DataFrame
+file_path = '/Users/suqiulin/Desktop/plp_practice_proj/reddit_keywords_results/reddit_keywords_full_distilbert.csv'
+df=pd.read_csv(file_path)
+
+st.title('Subreddit Keyword Frequency Analysis')
+
+# Filter out rows with sentiment 'POSITIVE' or 'NEUTRAL'
+filtered_df = df[~df['sentiment'].isin(['POSITIVE', 'NEUTRAL'])]
+
+# Convert 'created_utc' to datetime format
+
+df['created_utc'] = pd.to_datetime(df['created_utc'])
+min_date = df['created_utc'].min().date()
+max_date = df['created_utc'].max().date()
+start_date, end_date = st.date_input('Select a date range:', [min_date, max_date], min_value=min_date, max_value=max_date)
+
+# Step 2: Count the frequency of each keyword
+filtered_df = df[(df['created_utc'] >= pd.to_datetime(start_date)) & (df['created_utc'] <= pd.to_datetime(end_date))]
+filtered_df['keywords'] = filtered_df['keywords'].str.split(',')
+exploded_df = filtered_df.explode('keywords')
+
+# Step 3: Count the frequency of each keyword under each subreddit
+df1 = exploded_df.groupby(['subreddit', 'keywords']).size().reset_index(name='frequency')
+
+
+
+
+# Dropdown list to select a subreddit
+subreddit_list = df1['subreddit'].unique()
+selected_subreddit = st.selectbox('Select a subreddit to view top keywords:', subreddit_list)
+
+# Filter and display top 5 keywords based on frequency for the selected subreddit
+if selected_subreddit:
+    filtered_df1 = df1[df1['subreddit'] == selected_subreddit].sort_values(by='frequency', ascending=False)
+    top_keywords = filtered_df1.head(5)
+    
+    # Display the results
+    st.write(f"Top 5 keywords for subreddit: **{selected_subreddit}** from {start_date} to {end_date}")
+    st.dataframe(top_keywords)
