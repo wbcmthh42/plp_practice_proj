a7896259e12d7f1ee4308e53fadc4e5fd6564221
Tay Johnny <tayjohnny@Tays-MacBook-Pro.local>
2024-10-11 18:20:28 +0800

    add docs files to main

---
+++ docs/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'TechPulse'
+copyright = '2024, TechPulse Team'
+author = 'TechPulse Team'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode'  #optional
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+
+autodoc_mock_imports = ['datasets', 'bert_score', 'praw', 'nltk', 'prawcore', 'wordcloud', 'vaderSentiment']
+
+# Path to the logo file
+html_logo = 'source/_static/techpulse.png'
